77105ea7795dcb4e66bcc34370c656a00fdaeee3
cdrectenwald <cdrectenwald@gmail.com>
2020-01-22 18:24:36 -0600

    Create preprocess_inputs.py

---
+++ preprocess_inputs.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+def preprocessing(input_image, height, width):
+    '''
+    Given an input image, height and width:
+    - Resize to height and width
+    - Transpose the final "channel" dimension to be first
+    - Reshape the image to add a "batch" of 1 at the start 
+    '''
+    image = cv2.resize(input_image, (width, height))
+    image = image.transpose((2,0,1))
+    image = image.reshape(1, 3, height, width)
+
+    return image
+
+def text_detection(input_image):
+    '''
+    Given some input image, preprocess the image so that
+    it can be used with the related text detection model
+    you downloaded previously. You can use cv2.resize()
+    to resize the image.
+    '''
+    preprocessed_image = np.copy(input_image)
+
+    # TODO: Preprocess the image for the text detection model
+
+    return preprocessed_image
+
+
+def car_meta(input_image):
+    '''
+    Given some input image, preprocess the image so that
+    it can be used with the related car metadata model
+    you downloaded previously. You can use cv2.resize()
+    to resize the image.
+    '''
+    preprocessed_image = np.copy(input_image)
+
+    # TODO: Preprocess the image for the car metadata model
+
+    return preprocessed_image
